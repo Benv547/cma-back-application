dcb5f26095102cc0864dd2c840859038e81a80c4
Jean Mathieu <mathieu.jean88@wanadoo.fr>
2023-03-16 11:56:32 +0100

    start entity

---
+++ src/main/java/cma/cmabackapplication/Entity/Course.java
@@ -0,0 +1,17 @@
+import java.io.Serial;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course extends Serializable {
+    
+
+
+}
