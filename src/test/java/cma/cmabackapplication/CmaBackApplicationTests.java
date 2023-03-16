8513f5b97146c7ff4ecc5db1524c800c1b280d0b
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-03-16 10:09:58 +0100

    feat: add bootstrap app

---
+++ src/test/java/cma/cmabackapplication/CmaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package cma.cmabackapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
