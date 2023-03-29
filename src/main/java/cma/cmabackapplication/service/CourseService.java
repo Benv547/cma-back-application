0e2622298479c84973bd50023659d95729079356
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-03-29 14:30:11 +0200

    feat: add controller and service

---
+++ src/main/java/cma/cmabackapplication/service/CourseService.java
@@ -0,0 +1,25 @@
+package cma.cmabackapplication.service;
+
+import cma.cmabackapplication.entity.Course;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    public Course getCourseById(Long courseId) {
+        return null;
+    }
+
+    public Course createCourse(Course course) {
+        return null;
+    }
+
+    public Course updateCourse(Course course) {
+        return null;
+    }
+
+    public void deleteCourse(Long courseId) {
+    }
+}
