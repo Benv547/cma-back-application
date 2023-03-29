37501f65132b9cab6eb0bdd2a46222079441230d
Jean Mathieu <mathieu.jean88@wanadoo.fr>
2023-03-29 10:45:37 +0200

    Entity package

--- src/main/java/cma/cmabackapplication/Entity/Student.java
+++ src/main/java/cma/cmabackapplication/Entity/Student.java
@@ -6,18 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Course {
+public class Student {
     @Id
     private String id;
     private String name;
-    private String hourlyStart;
-    private String hourlyFinish;
-    private String room;
+    private String firstname;
+    private String email;
 }
