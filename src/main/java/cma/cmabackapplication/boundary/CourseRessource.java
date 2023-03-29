fbfaef51ef436f1cdc44c9286e8cb0fc5b06b5fe
Kyouis <45875365+Kyouis@users.noreply.github.com>
2023-03-29 17:23:50 +0200

    Update CourseRessource.java

--- src/main/java/cma/cmabackapplication/boundary/CourseRessource.java
+++ src/main/java/cma/cmabackapplication/boundary/CourseRessource.java
@@ -1,6 +1,6 @@
 package cma.cmabackapplication.boundary;
 
-import cma.cmabackapplication.Entity.Course;
+import cma.cmabackapplication.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRessource extends JpaRepository<Course, String> {
