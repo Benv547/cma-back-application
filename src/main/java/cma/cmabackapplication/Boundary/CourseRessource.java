c4d63b5104a9323fbf9b33ea738f2c1df01abdb7
Kyouis <45875365+Kyouis@users.noreply.github.com>
2023-03-29 16:40:29 +0200

    Delete src/main/java/cma/cmabackapplication/Boundary directory

--- src/main/java/cma/cmabackapplication/Boundary/CourseRessource.java
+++
@@ -1,7 +0,0 @@
-package cma.cmabackapplication.Boundary;
-
-import cma.cmabackapplication.Entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRessource extends JpaRepository<Course, String> {
-}
--- src/main/java/cma/cmabackapplication/Boundary/StudentRessource.java
+++
@@ -1,7 +0,0 @@
-package cma.cmabackapplication.Boundary;
-
-import cma.cmabackapplication.Entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRessource extends JpaRepository<Student, String> {
-}
