5121771c2a91e22f5d36a27245d092db301ba8fb
Jean Mathieu <mathieu.jean88@wanadoo.fr>
2023-03-29 14:54:48 +0200

    test entity

--- src/test/java/cma/cmabackapplication/CmaBackApplicationTests.java
+++ src/test/java/cma/cmabackapplication/CmaBackApplicationTests.java
@@ -1,5 +1,8 @@
 package cma.cmabackapplication;
 
+import cma.cmabackapplication.Boundary.CourseRessource;
+import cma.cmabackapplication.Entity.Course;
+import cma.cmabackapplication.Entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,14 @@ class CmaBackApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testEntity() {
+		Course c = new Course("id", "name", "start", "finish", "room");
+		Student s = new Student("id", "name", "fname", "email");
+		assert(c.getId().equals("id"));
+		assert(s.getId().equals("id"));
 	}
 
 }
